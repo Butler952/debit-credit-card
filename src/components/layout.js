b3c8c2aed3cd641883590736daaaa2438cad5183
aaronactivate <aaron@activate.co.uk>
2020-05-08 12:46:56 +0100

    Naming errors in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
+import Header from "./header"
 import Footer from "./Footer"
-import "./Layout.css"
+import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
