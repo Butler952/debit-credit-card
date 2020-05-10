f7d58614ba0ae248d0d9e912df2cf26d9e1ea015
aaronactivate <aaron@activate.co.uk>
2020-05-10 18:35:43 +0100

    Update handleSubmit

--- src/components/emailFormNetlify.js
+++ src/components/emailFormNetlify.js
@@ -1,69 +1,60 @@
-import React from "react"
+import React from 'react';
 import { navigate } from 'gatsby-link'
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
-
-export default class EmailForm extends React.Component {
-  state = {
-    firstName: "",
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
   }
 
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
+  class EmailForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { email: "" };
+    }
 
-    this.setState({
-      [name]: value,
-    })
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+      const form = e.target
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => navigate(form.getAttribute('action')))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+      const { email } = this.state;
+      return (
+        <form 
+          onSubmit={this.handleSubmit} 
+          className={this.props.className}
+          action="/page-2/"
+          method="post" 
+          data-netlify="true" 
+          data-netlify-honeypot="bot-field"
+          >
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="position-rel">
+            <input 
+              className="HeroInput"
+              placeholder={this.props.placeholder} 
+              type="text" 
+              name="email"
+              value={email}
+              onChange={this.handleChange}/>
+            <button className="btn btn-Secondary btn-input" type="submit">{this.props.buttoncta}</button>
+          </div>
+        </form>
+      );
+    }
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
-
-  render() {
-    return (
-      <form
-      name="sign-up-form"
-      method="POST"
-      action="/page-2/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      onSubmit={this.handleSubmit}
-      className={this.props.className}>
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={this.handleInputChange} />
-          </label>
-        </p>
-        <div className="position-rel">
-          <input
-            className="HeroInput"
-            placeholder={this.props.placeholder}
-            type="text"
-            name="email"
-            value={this.state.name}
-            onChange={this.handleInputChange}/>
-          <button className="btn btn-Secondary btn-input" type="submit">{this.props.buttoncta}</button>
-        </div>
-      </form>
-    )
-  }
-}
+  export default EmailForm
