c2247fa34299069dc63068877b9acb214ca7efd3
aaronactivate <aaron@activate.co.uk>
2020-05-10 17:34:36 +0100

    Update Netlify form to Gatsby

--- src/components/emailFormNetlify.js
+++ src/components/emailFormNetlify.js
@@ -1,23 +1,58 @@
-import React from "react"
+import React from 'react'
+import { navigate } from 'gatsby-link'
 
-export default class EmailForm extends React.Component {
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-  render() {
-    return (
-      <form 
-        name="sign-up-form" 
-        method="POST" 
-        data-netlify="true"
-        className={this.props.className}>
-        <div className="position-rel">
-          <input 
-            className="HeroInput"
-            placeholder={this.props.placeholder} 
-            type="text" 
-            name="email"/>
-          <button className="btn btn-Secondary btn-input" type="submit">{this.props.buttoncta}</button>
-        </div>
-      </form>
-    )
+export default function EmailForm() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  return (
+    <form
+      name="sign-up-form"
+      method="POST"
+      action="/page-2/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
+      className={this.props.className}>
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+        </label>
+      </p>
+      <div className="position-rel">
+        <input
+          className="HeroInput"
+          placeholder={this.props.placeholder}
+          type="text"
+          name="email"
+          onChange={handleChange} />
+        <button className="btn btn-Secondary btn-input" type="submit">{this.props.buttoncta}</button>
+      </div>
+    </form>
+  )
 }
