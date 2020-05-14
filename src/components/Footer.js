60feab649157d8ae5d601e3675dfbfdfc6e2fa4a
aaronactivate <aaron@activate.co.uk>
2020-05-14 21:10:49 +0100

    Add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 class Footer extends React.Component {
   constructor(props) {
@@ -11,6 +12,12 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <div className="Container">
+          <h3><Link to="/terms-and-conditions/">Terms and Conditions</Link></h3>
+          <h3><Link to="/privacy-policy/">Privacy Policy</Link></h3>
+          <h3><Link to="/cookie-policy/">Cookie Policy</Link></h3>
+        </div>
+        <br></br>
         <div className="Container footer">
           © Copyright GetBamboo {new Date().getFullYear()} <p className="badge badge-Secondary Caption ml-2 my-0" to="/page-2/">Alpha</p>
         </div>
